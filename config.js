6010b46eb9374b7e86cc716179c4fedadbb85dd3
Sans <002notsans@gmail.com>
2024-04-28 00:35:09 +0200

    🍃

--- config.js
+++ config.js
@@ -5,15 +5,17 @@ const twitch = ""; //tu met ton lien twitch ici
 const nsfw = ""; //tu met on ou off (on pour activer le nsfw et off pour le désactiver)
 const nitro_claimer = ""; //tu met on ou off (on pour activer l'auto claimer et off pour le désactiver)
 const multi_status = ["R$Z Project", "v2.0.0", "By Sans"]; //tu met le text que tu veut avoir en multi stream tu peut en mettre plus que 3 stv mais il faut bien mettre une virgule 
-const consolecolor = "3" // Couleur vives = 3 et couleur pales = 2 et très pales = 1
+const consolecolor = "" // Couleur vives = 3 et couleur pales = 2 et très pales = 1
+const premium = "" //Mettez votre clée premium ici pas obligé
 
 ////CONFIG RPC
 const imagerpc = ""; //tu met l'url de ton image rpc
+const appId = ""; //tu mets l'ID de l'application'
 const title = ""; //tu met le titre rpc
-const details = ""; //tu met le d============================================================================================================ etail rpc
+const details = ""; //tu met le detail rpc
 const state = ""; //tu met la state rpc
-const largetext = ""; //tu met le text de ta grande image
-const smalltext = ""; //tu met le text de ta petite image
+const bouton = ""; //tu mets le texte du bouton
+const url = ""; //tu mets l'url du bouton
 const sizeplayer = ""; //tu met le nombre de joueurs actuel (1 jusque à 999999999999)
 const sizeplayermax = "" //tu met le nombre de joueurs maximum (1 jusque à 999999999999)
 
@@ -33,14 +35,16 @@ module.exports = { //ne pas toucher
     twitch, //ne pas toucher
     nsfw, //ne pas toucher
     consolecolor, //ne pas toucher
+    premium, //ne pas toucher
     nitro_claimer, //ne pas toucher
     multi_status, //ne pas toucher
     imagerpc, //ne pas toucher
     title, //ne pas toucher
     details, //ne pas toucher
     state, //ne pas toucher
-    largetext, //ne pas toucher
-    smalltext, //ne pas toucher
     sizeplayer, //ne pas toucher
     sizeplayermax, //ne pas toucher
+    appId, //ne pas toucher
+    bouton, //ne pas toucher
+    url, //ne pas toucher
 } //ne pas toucher
